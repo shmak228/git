278d5f5a11afac8d8c4c2f871bfbe70db55ad7ae
shmak228 <62871434+shmak228@users.noreply.github.com>
2021-02-25 14:04:04 +0700

    boob added and new math

---
+++ booba.py
@@ -0,0 +1,5 @@
+import math
+
+x = math.sin(math.pi * 3 / 2)
+
+print(x)
--- python.py
+++ python.py
@@ -7,3 +7,10 @@ print(math.factorial(34))
 x /= 30
 
 print( x)
+
+if x > 1000000:
+    print('XYN')
+else:
+    x += 987654321
+    x = x / 444
+    print(x)
